b1d49742775c1d5d1a5d36cbdb39c8be032feca4
Slowres <AbdelrahmanAlasal@gmail.com>
2023-10-15 01:37:26 +0300

    Intial Controllers

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,56 @@
+class AuthorsController < ApplicationController
+
+
+
+
+   # ============ Views =================
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  # ============ CREATE - UPDATE -  DELETE =================
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def json_data
+
+  end
+
+
+  private
+
+  def rss_params
+
+  end
+
+  def set_rss
+
+  end
+
+
+
+
+end
